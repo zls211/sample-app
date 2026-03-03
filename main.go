5f6aeb7aebe664a519007841768e62135428d5b8
zls211 <student-02-afb4b75a2cf4@qwiklabs.net>
2026-03-03 17:24:18 +0000

    Add red endpoint and bump to v2.0

--- main.go
+++ main.go
@@ -25,8 +25,9 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/blue", blueHandler)
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/blue", blueHandler)
+    http.HandleFunc("/red", redHandler)
+    http.ListenAndServe(":8080", nil)
 }
 
 func blueHandler(w http.ResponseWriter, r *http.Request) {
@@ -35,3 +36,10 @@ func blueHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/png")
 	png.Encode(w, img)
 }
+func redHandler(w http.ResponseWriter, r *http.Request) {
+    img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+    draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
+    w.Header().Set("Content-Type", "image/png")
+    png.Encode(w, img)
+}
+
